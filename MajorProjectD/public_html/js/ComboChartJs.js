b9f0d2735b3786d2ba65d57e5b012418e6067d8a
gsangram17 <gsangrampatro1995@gmail.com>
2017-06-20 19:13:55 +0530

    first combo chart

--- MajorProjectD/public_html/js/ComboChartJs.js
+++ MajorProjectD/public_html/js/ComboChartJs.js
@@ -1,11 +1,17 @@
-
 $(document).ready(function() {
 dataset = [
-    {label:"90-91", "non-oil imports":18000, "Total imports":24000},
-    {label:"91-92", "non-oil imports":14000, "Total imports":19000},
-    {label:"92-93", "non-oil imports":16000, "Total imports":22000},
-    {label:"93-94", "non-oil imports":17000, "Total imports":23000}   
+    {label:"90-91", "non-oil imports":18000, "Total imports":24000  },
+    {label:"91-92", "non-oil imports":14000, "Total imports":19000  },
+    {label:"92-93", "non-oil imports":16000, "Total imports":22000  },
+    {label:"93-94", "non-oil imports":17000, "Total imports":23000 }   
 ];
+
+lineset= [
+    {label:"90-91", 'yxis':10},
+    {label:"91-92", 'yxis':40},
+    {label:"92-93", 'yxis':100 },
+    {label:"93-94", 'yxis':70 }
+]
 
 
 //auto selecting of width and height by netbeans-IDE
@@ -17,9 +23,6 @@ var margin = {top: (parseInt(d3.select('body').style('width'), 10)/10),
     width = parseInt(d3.select('body').style('width'), 10) - margin.left - margin.right,
     height =parseInt(d3.select('body').style('width'), 10)-margin.top-margin.bottom;
 
-//    console.log('HEIGHT @@@@',height);
-//    console.log('WIDTH ####',width);
-
 //setting the range 
 x0=d3.scale.ordinal()
         .rangeRoundBands([0,width],0.5);
@@ -28,39 +31,34 @@ x1=d3.scale.ordinal();
 
 y=d3.scale.linear().range([height,0]);
 
+y1=d3.scale.linear().range([height,0]);
+
+
 //colouring the grouped bar chart canvas
 var colorRange = d3.scale.category10();
 var color = d3.scale.ordinal()
     .range(colorRange.range());
-
-//create the svg canvas 
-var svg=d3.select('body')
-            .append('svg')
-            .attr('width' , width + margin.left + margin.right)
-            .attr('height' , height + margin.top + margin.bottom)
-          .style('background-color' , 'cyan')
-            .append('g')
-            .attr('transform','translate('+margin.left+','+margin.right+')');
   
     
 var options= d3.keys(dataset[0])
-            .filter(function(key) { return key !=='label'});
-//    console.log('options',options);
-dataset.forEach(function(d) { 
+            .filter(function(key) { return key !=='label'});                      
+//            console.log(options);
+    dataset.forEach(function(d) { 
     d.obs=options.map(function(name) {
-//        console.log('name',name);
-//        console.log('dname',+d[name]);
-        return {name: name, value: +d[name]}; });
+                    return {name: name, value: +d[name]}; });
+//             console.log(d.obs);
 })
     
    
     //assigning the domain of the plot
-    x0.domain(dataset.map(function(d) {   return d.label ;}));        
+    x0.domain(dataset.map(function(d) { return d.label ;}));
     x1.domain(options).rangeRoundBands([0, x0.rangeBand()]);
-    y.domain([0,24000]);
-    console.log('why x1',x0.rangeBand())   
-    
-    //scalling both axis
+    y.domain([0, 25000]);
+    y1.domain([0,100]);
+
+//    y1.domain([0,d3.max(dataset,function(d){   return d.yxis})])      
+                        
+//scalling both axis
 var xAxis=d3.svg.axis()
         .scale(x0)
         .orient('bottom');
@@ -68,6 +66,19 @@ var xAxis=d3.svg.axis()
 var yAxis=d3.svg.axis()
         .scale(y)
         .orient('left');
+
+var y1Axis=d3.svg.axis()
+        .scale(y1)
+        .orient('right');
+
+//create the svg canvas 
+var svg=d3.select('body')
+            .append('svg')
+            .attr('width' , width + margin.left + margin.right)
+            .attr('height' , height + margin.top + margin.bottom)
+            .style('background-color' , 'cyan')
+            .append('g')
+            .attr('transform','translate('+margin.left+','+margin.right+')');
 
 //creating x-axis
 svg.append('g')
@@ -87,6 +98,12 @@ svg.append('g')
         .attr('transform','translate('+margin.left+',0)')
         .call(yAxis);
 
+//creating y1-axis      
+svg.append('g')
+        .attr('class','y1 axis')
+        .attr('transform','translate('+width+',0)')
+        .call(y1Axis);
+
 var div=d3.select('body')
           .append('div')
           .attr('class','tooltip')
@@ -99,9 +116,39 @@ var bar=svg.selectAll('.bar')
         .append('g')
         .attr('class','rect')
         .attr("transform", function(d) { return "translate(" +x0(d.label) + ",0)"; });
-         
-        
-     //   
+
+//defining the line
+    var valueline=d3.svg.line()
+                   .x(function(d){ return x0(d.label)+47;})                                           
+                   .y(function(d){ return y1(d.yxis);});
+           
+     svg.append("path") 
+        .attr("d", valueline(lineset))
+        .attr("fill",'none')
+        .attr('stroke-width',3)
+        .attr('stroke','blue');
+
+svg.selectAll('dot')
+        .data(lineset)
+        .enter().append('circle')
+        .attr("r", 5)
+        .attr("cx",function(d){return x0(d.label)+47 ;})
+        .attr("cy",function(d){return y1(d.yxis) ;})
+        .on("mouseover", function(d) {	
+            div.transition()
+                .duration(200)
+                .style('opacity',0.9);
+            div.html( d.label + "<br/>"  + d.yxis)	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY ) + "px");          
+        })   
+        .on("mouseout", function(d) {		
+            div.transition()		
+                .duration(500)		
+                .style("opacity", 0);	      
+        });
+
+ 
     bar.selectAll("rect")
     .data(function(d) {  return d.obs; }) 
 //  console.log('object data',d.obs)                            
@@ -116,7 +163,8 @@ var bar=svg.selectAll('.bar')
     .style("fill", function(d) { return color(d.name); })
 
     .on('mouseover',function(d){ 
-                div.transition()
+//        console.log('d',d);
+              div.transition()
                  .duration(200)
                  .style('opacity',0.9),
               div.html(d.name + '<br/>'+'Qty in Million : '+d.value)
@@ -148,4 +196,4 @@ var legend = svg.selectAll(".legend")
       .attr("dy", ".35em")
       .style("text-anchor", "end")
       .text(function(d) { return d; });
-} );
+} )
